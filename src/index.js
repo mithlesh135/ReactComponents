f8358e305d903ff477f7b1174e9cc3478b37756c
Singh <singhmi@pun-singhmi-m.ds.ad.adp.com>
2018-09-20 21:16:34 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import MonthPicker from './components/MonthPicker';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<MonthPicker />, document.querySelector('div.container'));
